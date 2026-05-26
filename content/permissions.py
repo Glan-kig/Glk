d3b4401d97423db2c0062edd0ccd70c3932500f1
Vlan <23ik069si@esisalama.org>
2026-05-26 10:15:19 +0200

    Ajout des permissions

---
+++ content/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+
+class IsAdminOrEditor(BasePermission):
+    def has_permission(self, request: Request, view) -> bool:
+        return request.user.is_authentificated and request.user.role in ['admin', 'editor']
